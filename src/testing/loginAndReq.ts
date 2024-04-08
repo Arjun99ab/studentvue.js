74edc3d99f5609f6b201e238f4afb3c4c45ff59e
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2024-04-07 20:11:59 -0400

    loginandreq

--- src/testing/loginAndReq.ts
+++ src/testing/loginAndReq.ts
@@ -5,9 +5,9 @@ require('dotenv').config();
 const login = async() => {
     await loginVUE(process.env.USERNAME, process.env.PASSWORD, 'https://md-mcps-psv.edupoint.com/').then((client) => {
         console.log(client.cookieJar.toJSON().cookies[0].value);
-        client.getClasses().then((data) => {
-            console.log(JSON.stringify(data));
-        })
+        client.getClasses().then((classes) => {
+            console.log(JSON.stringify(classes));
+        });
     })
 };
 login();

59c2e5b3a4e1abbef7edaff78eb0cc8125f1731f
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-04-09 13:20:27 -0400

    headers

--- src/testing/StudentVue.ts
+++ src/testing/StudentVue.ts
@@ -94,6 +94,7 @@ function loginVUE(username: string, password: string, hostURL: string): Promise<
 
 class Client {
     private hostUrl: string;
+    private testmode = false;
     private username: string;
     private password: string;
     private schoolID = '';
@@ -107,9 +108,28 @@ class Client {
         jar: this.cookieJar,
         baseURL: 'http://localhost:3001/',
         withCredentials: true,
+        headers: (() => {
+            if (this.testmode) {
+                return {
+                    'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"', 
+                    'sec-ch-ua-mobile': '?0', 
+                    'sec-ch-ua-platform': '"macOS"', 
+                    'Upgrade-Insecure-Requests': '1', 
+                    'Connection': 'keep-alive', 
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', 
+                    'Sec-Fetch-Site': 'same-origin', 
+                    'Sec-Fetch-User': '?1', 
+                    'host': 'md-mcps-psv.edupoint.com', 
+                }
+            }
+            return {}
+        })()
     }));
 
-    constructor(username: string, password: string,  hostUrl: string) {
+    constructor(username: string, password: string, hostUrl: string, testmode?: boolean) {
+        if (!(testmode == null)) {
+            this.testmode = testmode;
+        }
         this.hostUrl = hostUrl;
         this.username = username;
         this.password = password;
@@ -124,9 +144,23 @@ class Client {
             'ctl00$MainContent$username': this.username
         };
         const loginConfig = {
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        'Content-Type': 'application/x-www-form-urlencoded', 
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
+                        'Sec-Fetch-Mode': 'navigate', 
+                        'Sec-Fetch-User': '?1', 
+                        'Sec-Fetch-Dest': 'document', 
+                        'host': 'md-mcps-psv.edupoint.com', 
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True'}
+            })()
         };
         await this.client.post("", loginData, loginConfig).then(({ config }) => {
                 if (config.jar) {
@@ -141,9 +175,25 @@ class Client {
         const gradebookConfig = {
             jar: this.cookieJar,
             withCredentials: true,
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/GradebookFocusClassInfo'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/GradebookFocusClassInfo'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        'Content-Type': 'application/json; charset=UTF-8', 
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
+                        'Sec-Fetch-Mode': 'navigate', 
+                        'Sec-Fetch-User': '?1', 
+                        'Sec-Fetch-Dest': 'document', 
+                        'host': 'md-mcps-psv.edupoint.com', 
+                        'Origin': 'https://md-mcps-psv.edupoint.com', 
+                        'Referer': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0',
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/GradebookFocusClassInfo'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/GradebookFocusClassInfo'}
+            })()
         };
         return new Promise((resolve, reject) => {
             this.client.post("", gradebookData, gradebookConfig).then(({ data }) => {
@@ -164,18 +214,53 @@ class Client {
         const loadControlConfig = {
             jar: this.cookieJar,
             withCredentials: true,
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        'Content-Type': 'application/json; charset=UTF-8', 
+                        'Accept': 'application/json, text/javascript, */*; q=0.01', 
+                        'Sec-Fetch-Mode': 'cors', 
+                        'Sec-Fetch-User': '?1', 
+                        'Sec-Fetch-Dest': 'empty', 
+                        'Sec-Fetch-Site': 'same-origin', 
+                        'host': 'md-mcps-psv.edupoint.com', 
+                        'Origin': 'https://md-mcps-psv.edupoint.com', 
+                        'Referer': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0',
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'}
+            })()
         }
 
         const classData = '{"FriendlyName":"genericdata.classdata","Method":"GetClassData","Parameters":"{}"}';
         const classConfig = {
             jar: this.cookieJar,
             withCredentials: true,
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        'Content-Type': 'application/json; charset=UTF-8', 
+                        'Accept': 'application/json, text/javascript, */*; q=0.01', 
+                        'Accept-Language': 'en-US,en;q=0.9',
+                        'host': 'md-mcps-psv.edupoint.com', 
+                        'Origin': 'https://md-mcps-psv.edupoint.com', 
+                        'Referer': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0',
+                        'Sec-Fetch-User': '?1', 
+                        'Sec-Fetch-Dest': 'empty', 
+                        'Sec-Fetch-Mode': 'cors', 
+                        'Sec-Fetch-Site': 'same-origin', 
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'}
+            })()
         }
         return new Promise((resolve, reject) => {
             this.client.post('', loadControlData, loadControlConfig).then(() => {
@@ -193,18 +278,53 @@ class Client {
         const loadControlConfig = {
             jar: this.cookieJar,
             withCredentials: true,
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        'Content-Type': 'application/json; charset=UTF-8', 
+                        'Accept': 'application/json, text/javascript, */*; q=0.01', 
+                        'Sec-Fetch-Mode': 'cors', 
+                        'Sec-Fetch-User': '?1', 
+                        'Sec-Fetch-Dest': 'empty', 
+                        'Sec-Fetch-Site': 'same-origin', 
+                        'host': 'md-mcps-psv.edupoint.com', 
+                        'Origin': 'https://md-mcps-psv.edupoint.com', 
+                        'Referer': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0',
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/service/PXP2Communication.asmx/LoadControl'}
+            })()
         }
 
         const classData = '{"FriendlyName":"pxp.course.content.items","Method":"LoadWithOptions","Parameters":"{\\"loadOptions\\":{\\"sort\\":[{\\"selector\\":\\"due_date\\",\\"desc\\":false}],\\"filter\\":[[\\"isDone\\",\\"=\\",false]],\\"group\\":[{\\"Selector\\":\\"Week\\",\\"desc\\":false}],\\"requireTotalCount\\":true,\\"userData\\":{}},\\"clientState\\":{}}"}';
         const classConfig = {
             jar: this.cookieJar,
             withCredentials: true,
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        'Content-Type': 'application/json; charset=UTF-8', 
+                        'Accept': 'application/json, text/javascript, */*; q=0.01', 
+                        'Accept-Language': 'en-US,en;q=0.9',
+                        'host': 'md-mcps-psv.edupoint.com', 
+                        'Origin': 'https://md-mcps-psv.edupoint.com', 
+                        'Referer': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0',
+                        'Sec-Fetch-User': '?1', 
+                        'Sec-Fetch-Dest': 'empty', 
+                        'Sec-Fetch-Mode': 'cors', 
+                        'Sec-Fetch-Site': 'same-origin', 
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'}
+            })()
         }
         return new Promise((resolve, reject) => {
             this.client.post('', loadControlData, loadControlConfig).then(() => {
@@ -221,9 +341,24 @@ class Client {
         const gradebookConfig = {
             jar: this.cookieJar,
             withCredentials: true,
-            headers: {
-                'Target-URL': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0'
-            }
+            // headers: {
+            //     'Target-URL': 'https://md-mcps-psv.edupoint.com/PXP2_GradeBook.aspx?AGU=0'
+            // }
+            headers: (() => {
+                if (this.testmode) {
+                    return {
+                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                        "Host": "md-mcps-psv.edupoint.com",
+                        "Referer": "https://md-mcps-psv.edupoint.com/PXP2_LaunchPad.aspx",
+                        "Sec-Fetch-Dest": "document",
+                        "Sec-Fetch-Mode": "navigate",
+                        "Sec-Fetch-Site": "same-origin",
+                        "Sec-Fetch-User": "?1",
+                        'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'
+                    }
+                }
+                return {'Target-URL': 'https://md-mcps-psv.edupoint.com/api/GB/ClientSideData/Transfer?action=genericdata.classdata-GetClassData'}
+            })()
         }
         return new Promise<void>((resolve, reject) => {
             this.client.get('', gradebookConfig).then(({ data }) => {
